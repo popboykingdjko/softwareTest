6a6aea8c0239e058931bdf8bfb47cad4f81c0ca0
popboykingdjko <38071856+popboykingdjko@users.noreply.github.com>
2019-03-26 10:59:06 +0800

    Add files via upload

---
+++ homework3/test/cn/tjuscs/st/TestPrintPrimes.java
@@ -0,0 +1,12 @@
+package cn.tjuscs.st;
+
+import org.junit.Test;
+
+public class TestPrintPrimes {
+   @Test
+   public void test(){
+	   String [] argv = new String [1];
+	   argv[0] = "3";
+	   PrintPrimes.main(argv);
+   }
+}
